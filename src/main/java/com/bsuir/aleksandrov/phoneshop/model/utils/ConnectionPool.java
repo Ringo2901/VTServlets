15ecf52382f30373fd4059b2b36a4f67f6aa0fa9
Никита <92538968+Ringo2901@users.noreply.github.com>
2023-11-05 19:45:10 +0300

    Making ConnectionPool thread safe

--- src/main/java/com/bsuir/aleksandrov/phoneshop/model/utils/ConnectionPool.java
+++ src/main/java/com/bsuir/aleksandrov/phoneshop/model/utils/ConnectionPool.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * Connection pool to work with database
  * @author nekit
@@ -41,6 +44,7 @@ public class ConnectionPool {
      * List of connection in use
      */
     private final List<Connection> usedConnections = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     /**
      * Realisation of Singleton pattern
@@ -74,18 +78,23 @@ public class ConnectionPool {
      * @throws SQLException throws when fail to connect to database
      */
     public synchronized Connection getConnection() throws SQLException {
-        if (connectionPool.isEmpty()) {
-            if (usedConnections.size() < maxConnections) {
-                Connection connection = createConnection();
+        lock.readLock().lock();
+        try {
+            if (connectionPool.isEmpty()) {
+                if (usedConnections.size() < maxConnections) {
+                    Connection connection = createConnection();
+                    usedConnections.add(connection);
+                    return connection;
+                } else {
+                    throw new SQLException("Reached maximum connections limit.");
+                }
+            } else {
+                Connection connection = connectionPool.remove(connectionPool.size() - 1);
                 usedConnections.add(connection);
                 return connection;
-            } else {
-                throw new SQLException("Reached maximum connections limit.");
             }
-        } else {
-            Connection connection = connectionPool.remove(connectionPool.size() - 1);
-            usedConnections.add(connection);
-            return connection;
+        } finally {
+            lock.readLock().unlock();
         }
     }
 
@@ -94,8 +103,13 @@ public class ConnectionPool {
      * @param connection connection to release
      */
     public synchronized void releaseConnection(Connection connection) {
-        usedConnections.remove(connection);
-        connectionPool.add(connection);
+        lock.writeLock().lock();
+        try {
+            usedConnections.remove(connection);
+            connectionPool.add(connection);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     /**
@@ -115,13 +129,18 @@ public class ConnectionPool {
      * @throws SQLException throws when fail to close
      */
     public void closeAllConnections() throws SQLException {
-        for (Connection connection : connectionPool) {
-            connection.close();
+        lock.writeLock().lock();
+        try {
+            for (Connection connection : connectionPool) {
+                connection.close();
+            }
+            connectionPool.clear();
+            for (Connection connection : usedConnections) {
+                connection.close();
+            }
+            usedConnections.clear();
+        } finally {
+            lock.writeLock().unlock();
         }
-        connectionPool.clear();
-        for (Connection connection : usedConnections) {
-            connection.close();
-        }
-        usedConnections.clear();
     }
 }
